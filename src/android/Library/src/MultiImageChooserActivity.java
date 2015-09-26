c9d304cc738174df04c8bf65851e2385f02e58d2
Aapo Kuuselo <aapo.kuuselo@gmail.com>
2015-09-25 23:37:15 -0500

    Added java.nio.file.spi.FileTypeDetector

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -32,6 +32,7 @@ package com.synconset;
 
 import java.net.URI;
 import java.io.File;
+import java.nio.file.spi.FileTypeDetector;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
